741338a3a82ecd13a5892ed39a66dfe8062e0b0d
Leonid <rockwil91@gmail.com>
2022-11-19 16:00:57 +0200

    Д/З 5 - Задача 38

--- task 2/Program.cs
+++ task 2/Program.cs
@@ -3,15 +3,36 @@
 
 [3 7 22 2 78] -> 76*/
 
-int size = 5;
-int count = 0;
-int[] array = new int[size];
+int size = 10;
+double[] array = new double[size];
+double minArr = 100;
+double maxArr = 0;
 
 for (int i = 0; i < size; i++)
 {
-    array[i] = new Random().Next(1, 1000);
-    if (array[i] % 2 == 0)
+    array[i] = new Random().NextDouble() * 100;
+    array[i] = Math.Round(array[i], 2);
+}
+
+void PrintArray (double[] arrayNum)
+{
+    for (int i = 0; i < size; i++)
     {
-        count++;
+        Console.Write($"{arrayNum[i]} ");
     }
 }
+
+PrintArray(array);
+
+for (int i = 0; i < size; i++)
+{
+    if (array[i] < minArr)
+        minArr = array[i];
+}
+
+for (int i = 0; i < size; i++)
+{
+    if (array[i] > maxArr)
+        maxArr = array[i];
+}
+Console.Write($"-> {Math.Round(maxArr - minArr, 2)}");
